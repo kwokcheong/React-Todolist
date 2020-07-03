ed08c35f24b4eeafd7caa4c3b8c7839fa7bab1bb
Kwok cheong <wongkwokcheong@hotmail.com>
2020-07-03 18:10:46 +0800

    Converted singletodo class into a function component

--- todolist/src/SingleTodo.js
+++ todolist/src/SingleTodo.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react'; 
 
-export default class SingleTodo extends Component{
-    //We will be looking at props, rather than state
-    // State is internal data while props are external data coming in from other sources/components 
+// export default class SingleTodo extends Component{
+//     //We will be looking at props, rather than state
+//     // State is internal data while props are external data coming in from other sources/components \
 
-    constructor(){
-        super();
-    }
+//     //Props are external quantities handed to the component, the component cannot change these
 
-    render(){
-        return(
-            //This props have no idea what todo is or delete is, we need the parent/Global source to send over what those variables do.
-        <li>{this.props.todo}<button onClick={this.props.delete}>X</button></li>
-        )
-    }
+//     constructor(){
+//         super();
+//     }
+
+//     render(){
+//         return(
+//             //This props have no idea what todo is or delete is, we need the parent/Global source to send over what those variables do.
+//         <li>{this.props.todo}<button onClick={this.props.delete}>X</button></li>
+//         )
+//     }
+// }
+
+
+//FUNCTIONAL COMPONENT
+//No need "this" anymore, just takes in a single parameter props
+//Only accepts props and no internal state
+
+const SingleTodo = (props) =>{
+    return (
+        <li>{props.todo}<button onClick={props.delete}>X</button></li>
+    )
 }
+
+export default SingleTodo;
